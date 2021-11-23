a8c8d38a04e37f606c36eb74ba674ec22fde11be
Maxim <58148359+bistenofficial@users.noreply.github.com>
2021-11-23 12:34:59 +0300

    Часть UI готова

--- app/src/main/java/com/example/coffein/MainActivity.java
+++ app/src/main/java/com/example/coffein/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.coffein;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -16,6 +19,8 @@ private SliderAdapter sliderAdapter;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 //
 //        mSlidePager = (ViewPager) findViewById(R.id.slideViewPager);
@@ -23,5 +28,11 @@ private SliderAdapter sliderAdapter;
 //
 //        sliderAdapter = new SliderAdapter(this);
 //        mSlidePager.setAdapter(sliderAdapter);
+    }
+
+    public void goToQR(View view)
+    {
+        Intent intent = new Intent(this, SplashActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/coffein/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.coffein;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import java.util.concurrent.TimeUnit;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1500);
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
